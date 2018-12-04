593174e18ea5cf9ef7ba6d2aaf2f73bb5b92499d
Mike McKee <mikecm@gmail.com>
2018-12-04 12:04:36 -0700

    Add further configuration and root route to server file
    We use the bodyParser module to automatically parse JSON into JS objects from
    incoming responses from our requests. We set the port to 3000 if it is otherwise
    not set in the environment variables. Finally, we listen continuously to check
    for incoming connections to any of the routes listed above line 18.

--- server.js
+++ server.js
@@ -5,3 +5,16 @@ const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Play BE';
+
+app.get('/', (request, response) => {
+  response.send('Play Back End');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
