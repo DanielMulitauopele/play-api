8c3cb5bc5ac59c40bd240dd23bbfc6ad5aa13d08
Mike McKee <mikecm@gmail.com>
2018-12-05 15:25:10 -0700

    Add export for testing, test environment set
    In our server.js file, I've added the export on line 67 to make sure we
    can export the bulk of the app commands so we can run the testing suite
    on them.
    
    In the testing file, I have set the environment variable to test so
    that it uses the test database instead of the development environment
    database.

--- server.js
+++ server.js
@@ -64,3 +64,5 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
+module.exports = app;
+
--- test/playlist_spec.js
+++ test/playlist_spec.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
@@ -8,18 +10,14 @@ chai.use(chaiHttp);
 describe('POST /api/v1/playlists', () => {
   it('should create a new playlist', done => {
     chai.request(server)
-      // Notice the change in the verb
       .post('/api/v1/playlists')
-      // Here is the information sent in the body or the request
       .send({
-        playlist_name: 'Test Playlist'
+        playlist_name: 'Test Playlist 2'
       })
       .end((err, response) => {
-        // Different status here
         response.should.have.status(201);
         response.body.should.be.a('object');
-        response.body.should.have.property('id');
-        response.body.should.have.property('playlist_name');
+        response.body.should.have.property('playlist');
         done();
     });
   });
