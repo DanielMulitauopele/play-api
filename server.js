13783156f7af09f2afaaee97ba2648e9e0dbfe6d
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-05 15:42:20 -0700

    Adds get route for songs (favorites)

--- server.js
+++ server.js
@@ -42,7 +42,15 @@ app.post('/api/v1/songs', (request, response) => {
     });
 });
 
-
+app.get('/api/v1/songs', (request, response) => {
+  database('songs').select()
+  .then((songs) => {
+    response.status(200).json(songs);
+  })
+  .catch((error) => {
+    response.status(500).json({error});
+  })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
