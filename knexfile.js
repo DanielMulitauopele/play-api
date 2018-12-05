7c84a305f90be7c12707cb4a60486a5f99afb098
Mike McKee <mikecm@gmail.com>
2018-12-05 12:22:17 -0700

    Add config for database connection for Heroku
    This adds the correct connection URL for our database when on production
    while deployed to Heroku. Heroku also requires ssl to be set to true
    in our database settings. Client: pg works for some reason over
    postgresql even though they should be synonymous, not sure why, seems
    to be Heroku specific.

--- knexfile.js
+++ knexfile.js
@@ -12,19 +12,16 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: 'postgres://posdbbnvjqqogr:c228eb64e7c78be61324792ea4b02206a51455a7cb28d1ea4a8c5b61e5a1b210@ec2-23-21-201-12.compute-1.amazonaws.com:5432/d4oneut0sskh52',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    ssl: true
   }
 
 };
