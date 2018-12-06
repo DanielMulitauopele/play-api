5f7717b5698fa3d9b139728b6dbabbf2dc637038
Mike McKee <mikecm@gmail.com>
2018-12-05 18:09:24 -0700

    Update happy path test for adding playlists

--- test/playlist_spec.js
+++ test/playlist_spec.js
@@ -5,9 +5,32 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const knex = require('knex')(configuration);
+
 chai.use(chaiHttp);
 
 describe('POST /api/v1/playlists', () => {
+  beforeEach((done) => {
+    knex.migrate.rollback()
+      .then(() => {
+        knex.migrate.latest()
+          .then(() => done())
+          .catch(error => {
+            throw error;
+          });
+      });
+  });
+
+  afterEach((done) => {
+    knex.migrate.rollback()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+      });
+  });
+
   it('should create a new playlist', done => {
     chai.request(server)
       .post('/api/v1/playlists')
@@ -16,8 +39,12 @@ describe('POST /api/v1/playlists', () => {
       })
       .end((err, response) => {
         response.should.have.status(201);
+        response.should.have.be.json;
         response.body.should.be.a('object');
         response.body.should.have.property('playlist');
+        response.body['playlist'].should.have.property('id');
+        response.body['playlist'].should.have.property('playlist_name');
+        response.body['playlist']['playlist_name'].should.equal('Test Playlist 2');
         done();
     });
   });
