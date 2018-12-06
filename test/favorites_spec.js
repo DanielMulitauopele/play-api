1ef1f00be121fda39009970d4d1e5810738e808f
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-06 13:11:13 -0700

    Pulls down Mike's code, adds seed files, tests pass

--- test/favorites_spec.js
+++ test/favorites_spec.js
@@ -16,21 +16,16 @@ describe('GET /api/v1/songs', () => {
     database.migrate.rollback()
       .then(() => {
         database.migrate.latest()
-          .then(() => done())
-          .catch(error => {
-            throw error;
+          .then(() => {
+            database.seed.run()
+            .then(() => done())
+            .catch(error => {
+              throw error;
+            });
           });
       });
   });
 
-  afterEach((done) => {
-    database.migrate.rollback()
-      .then(() => done())
-      .catch(error => {
-        throw error;
-      });
-  });
-  
   it('should return all favorited songs', done => {
     chai.request(server)
     .get('/api/v1/songs')
@@ -40,21 +35,10 @@ describe('GET /api/v1/songs', () => {
       done();
     });
   });
-});
 
-describe('GET /api/v1/songs', () => {
   it('should return song by id', done => {
-    let new_song = {
-      "id": 1,
-      "name": "We Will Rock You",
-      "artist_name": "Queen",
-      "genre": "Rock",
-      "song_rating": 88
-    }
-
-    database('songs').insert(new_song)
     chai.request(server)
-    .get(`/api/v1/songs/${new_song.id}`)
+    .get('/api/v1/songs/2')
     .end((error, response) => {
       response.should.have.status(200);
       response.should.be.json;
@@ -62,25 +46,3 @@ describe('GET /api/v1/songs', () => {
     })
   });
 });
-
-// describe('/GET/:id book', () => {
-//       it('it should GET a book by the given id', (done) => {
-//           let book = new Book({ title: "The Lord of the Rings", author: "J.R.R. Tolkien", year: 1954, pages: 1170 });
-//           book.save((err, book) => {
-//               chai.request(server)
-//             .get('/book/' + book.id)
-//             .send(book)
-//             .end((err, res) => {
-//                   res.should.have.status(200);
-//                   res.body.should.be.a('object');
-//                   res.body.should.have.property('title');
-//                   res.body.should.have.property('author');
-//                   res.body.should.have.property('pages');
-//                   res.body.should.have.property('year');
-//                   res.body.should.have.property('_id').eql(book.id);
-//               done();
-//             });
-//           });
-//
-//       });
-//   });
