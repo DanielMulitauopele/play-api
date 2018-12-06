fd7bad1ca627041ca23e7f883fa680475ccaeb56
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-12-05 18:15:49 -0700

    Adds before blocks back in, was forgetting to add the dev, config, and env variables

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,7 +3,19 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 chai.use(chaiHttp);
+
+before((done) => {
+  database.migrate.latest()
+    .then(() => done())
+    .catch(error => {
+      throw error;
+    });
+});
 
 describe('GET /api/v1/songs', () => {
   it('should return all favorited songs', done => {
