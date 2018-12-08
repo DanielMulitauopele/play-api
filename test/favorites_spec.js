d381fc8f01f854a3ee6903c886bfc92704daf22b
Mike McKee <mikecm@gmail.com>
2018-12-08 16:55:29 -0700

    Refactor delete song and get song tests

--- test/favorites_spec.js
+++ test/favorites_spec.js
@@ -14,23 +14,40 @@ chai.use(chaiHttp);
 describe('API Songs Endpoint', () => {
   describe('DELETE /api/v1/songs/:id', () => {
     it('should delete a song', done => {
-      chai.request(server)
-        .delete('/api/v1/songs/1')
-        .end((err, response) => {
-          response.should.have.status(204);
-          done();
-        });
+      let songCount;
+
+      database('songs').select()
+        .then(songs => {
+          songCount = songs.length;
+        })
+        .then(() => {
+          chai.request(server)
+            .delete('/api/v1/songs/1')
+            .end((err, response) => {
+              response.should.have.status(204);
+              songCount--;
+            });
+        })
+        .then(() => {
+          database('songs').select()
+            .then(songs => {
+              songs.length.should.equal(songCount);
+            })
+            .catch(error => console.error({ error }));
+        })
+        .catch(error => console.error({ error }));
+      done();
     });
 
     it('should return an error if song does not exist', done => {
       chai.request(server)
-        .delete('/api/v1/songs/4')
+        .delete('/api/v1/songs/5000')
         .end((err, response) => {
           response.should.have.status(404);
           response.should.be.json;
           response.should.be.a('Object');
           response.body.should.have.property('message');
-          response.body.message.should.equal('Song with ID 4 not found');
+          response.body.message.should.equal('Song with ID 5000 not found');
           done();
         })
     });
@@ -39,10 +56,17 @@ describe('API Songs Endpoint', () => {
   describe('GET /api/v1/songs', () => {
     it('should return all favorited songs', done => {
       chai.request(server)
-      .get('/api/v1/songs')
+      .get('/api/v1/favorites')
       .end((error, response) => {
         response.should.have.status(200);
         response.should.be.json;
+        response.should.be.a('Object')
+        response.body.should.be.a('Array');
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('name');
+        response.body[0].should.have.property('artist_name');
+        response.body[0].should.have.property('genre');
+        response.body[0].should.have.property('song_rating');
         done();
       });
     });
