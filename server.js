914112bed2bdc425cd4238979db1759a0a513985
Mike McKee <mikecm@gmail.com>
2018-12-08 19:31:15 -0700

    Removed unneeded error handlers

--- server.js
+++ server.js
@@ -36,9 +36,6 @@ app.post('/api/v1/songs', (request, response) => {
   database('songs').insert(song, ['id', 'name', 'artist_name', 'genre', 'song_rating'])
     .then(song => {
       response.status(201).json({ songs: song[0] })
-    })
-    .catch(error => {
-      response.status(500).json({ error });
     });
 });
 
@@ -46,9 +43,6 @@ app.get('/api/v1/favorites', (request, response) => {
   database('songs').select(['id', 'name', 'artist_name', 'genre', 'song_rating'])
   .then((songs) => {
     response.status(200).json(songs);
-  })
-  .catch((error) => {
-    response.status(500).json({error});
   });
 });
 
@@ -62,10 +56,7 @@ app.get('/api/v1/songs/:id', (request, response) => {
           error: `Could not find song with id: ${request.params.id}`
         })
       }
-    })
-    .catch(error => {
-      response.status(500).json({error});
-    })
+    });
 })
 
 app.get('/api/v1/playlists/:id/songs', (request, response) => {
@@ -77,22 +68,18 @@ app.get('/api/v1/playlists/:id/songs', (request, response) => {
     .then(playlists => {
       if(playlists.length) {
         playlistResponse = playlists[0];
+        database('playlist_songs').where('playlist_id', playlistId)
+          .select(['songs.id', 'name', 'artist_name', 'genre', 'song_rating'])
+          .join('songs', {'songs.id': 'playlist_songs.song_id'})
+          .then(songs => {
+            playlistResponse["songs"] = songs;
+            response.status(200).json(playlistResponse);
+          });
       } else {
         response.status(404).json({ error: `Playlist with ID ${playlistId} does not exist` });
       }
-    })
-    .then(() => {
-      database('playlist_songs').where('playlist_id', playlistId)
-        .select(['songs.id', 'name', 'artist_name', 'genre', 'song_rating'])
-        .join('songs', {'songs.id': 'playlist_songs.song_id'})
-        .then(songs => {
-          playlistResponse["songs"] = songs;
-          response.status(200).json(playlistResponse);
-        });
     });
 });
-
-pry = require('pryjs');
 
 app.get('/api/v1/playlists', (request, response) => {
   database.raw(`SELECT playlists.id, playlists.playlist_name, array_agg(json_build_object('id', songs.id, 'name', songs.name, 'artist_name', songs.artist_name, 'genre', songs.genre, 'song_rating', songs.song_rating)) as songs
@@ -103,9 +90,6 @@ app.get('/api/v1/playlists', (request, response) => {
                 ORDER BY playlists.id`)
           .then(playlists => {
             response.status(200).json(playlists.rows);
-          })
-          .catch(error => {
-            response.status(500).send({ error })
           });
 });
 
@@ -121,9 +105,6 @@ app.post('/api/v1/playlists', (request, response) => {
   database('playlists').insert(playlist, ['id', 'playlist_name'])
     .then(playlist => {
       response.status(201).json({ playlist: playlist[0] })
-    })
-    .catch(error => {
-      response.status(500).json({ error })
     });
 });
 
@@ -136,16 +117,10 @@ app.delete('/api/v1/songs/:id', (request, response) => {
         database('songs').where('id', songId).del()
           .then(() => {
             response.status(204).end();
-          })
-          .catch(error => {
-            response.status(500).send({ error });
           });
       } else {
         response.status(404).json({ message: `Song with ID ${songId} not found` });
       }
-    })
-    .catch(error => {
-      response.status(500).send({ error });
     });
 });
 
@@ -179,15 +154,9 @@ app.post('/api/v1/playlists/:playlist_id/songs/:id', (request, response) => {
             response.status(201).json({
               message: `Successfully added ${songName} to ${playlistName}`
             });
-          })
-          .catch(error => {
-            response.status(500).send({ error });
           });
       }
       });
-    })
-    .catch(error => {
-      response.status(500).send({ error });
     });
 });
 
