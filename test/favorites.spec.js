ead0a50b27724b1b47884845dc64d5ec605d200a
Mike McKee <mikecm@gmail.com>
2018-12-10 16:52:28 -0700

    Add beforeEach hook to favorites spec test

--- test/favorites.spec.js
+++ test/favorites.spec.js
@@ -12,6 +12,20 @@ const database = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('API Songs Endpoint', () => {
+  beforeEach((done) => {
+    database.migrate.rollback()
+      .then(() => {
+        database.migrate.latest()
+          .then(() => {
+            return database.seed.run()
+            .then(() => done())
+            .catch(error => {
+              throw error;
+            });
+          });
+      });
+  });
+  
   describe('PATCH /api/v1/songs/:id', () => {
     it('should update a song', done => {
       chai.request(server)
